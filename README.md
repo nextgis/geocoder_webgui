b4c7136fb3f305a0de84e6b2fc6cdd404d439fab
Kolesov Dmitry <kolesov.dm@gmail.com>
2016-04-07 15:58:41 +0300

    Use address_utils

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Commercial support
 Need to fix a bug or add a feature to Geocoder_Webgui? We provide custom development and support for this software. [Contact us](http://nextgis.ru/en/contact/) to discuss options!
 
 [![http://nextgis.com](http://nextgis.ru/img/nextgis.png)](http://nextgis.com)
+
+
+Install
+-------
+
+```
+git clone git@github.com:nextgis/address_utils.git
+cd addres_utils
+python setup.py develop
+cd ../geocoder_webgui
+python setup.py develop
+```
--- webgui/webgui/views.py
+++ webgui/webgui/views.py
@@ -1,13 +1,36 @@
-from pyramid.view import view_config
+# encoding: utf-8
 
+from pyramid.view import view_config
+from sqlalchemy import create_engine
+
+from address_utils.postgres.models import Name
+from address_utils.postgres.models import Base, DBSession
+
+
+# FIXME: move connection string to config file
+connection_string = 'postgresql://geocoder:@localhost:5432/geocoder'
+engine = create_engine(connection_string)
+
+Base.metadata.bind = engine
+DBSession.configure(bind=engine)
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'webgui'}
 
 @view_config(route_name='address_parser', renderer='templates/address_parser.pt')
-def my_view(request):
+def address_parser(request):
     params = dict((k.upper(), v) for k, v in request.params.iteritems())
-    input_address = params.get('ADDRESS')
-    x = 'Address: ' + input_address
-    return {'input_address': input_address, 'address': x}
+
+    input_address = unicode(params.get('ADDRESS'))
+    count = params.get('COUNT')
+    try:
+        count = int(count)
+    except (ValueError, TypeError) as e:
+        count = 5
+
+    # import ipdb; ipdb.set_trace()
+    addresses = Name.extract_addresses(input_address)
+    addresses = [{'addr': a[0], 'count': a[1]} for a in addresses.most_common(count)]
+
+    return {'input_address': input_address, 'address': addresses}
